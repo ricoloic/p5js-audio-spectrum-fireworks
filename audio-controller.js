5ae8ef1b677cd6063d841cfc25274ed469f6af4c
ricoloic <ricoloic4@gmail.com>
2024-09-07 22:41:12 -0400

    updated sketch with better visual

---
+++ audio-controller.js
@@ -0,0 +1,312 @@
+/**
+ * @async
+ * @param {string} url
+ * @return {Promise<ArrayBuffer | null>}
+ */
+async function fetchArrayBuffer(url) {
+    return new Promise((resolve) => {
+      const request = new XMLHttpRequest();
+      request.open('GET', url);
+      request.responseType = 'arraybuffer';
+  
+      request.onload = () => {
+        if (request.status >= 200 && request.status < 300 && request.response) {
+          resolve(request.response);
+        } else {
+          resolve(null);
+        }
+      };
+  
+      request.onerror = function () {
+        resolve(null);
+      };
+  
+      request.send();
+    });
+  }
+  
+  /**
+   * @typedef {32 | 64 | 128 | 256 | 512 | 1024 | 2048 | 4096 | 8192 | 16384 | 32768} FftSize
+   */
+  
+  class AudioController {
+    constructor() {
+      this.context = null;
+  
+      this.buffer = null;
+      this.source = null;
+      this.analyzer = null;
+      this.gain = null;
+      this.convolver = null;
+  
+      this.undecodedBuffer = null;
+  
+      this.loaded = false;
+      this.initialized = false;
+  
+      this.started = false;
+  
+      this.realPausedTime = 0;
+      this.realStartTime = 0;
+    }
+  
+    /**
+     * @async
+     * @method load
+     * @methodOf AudioController
+     * @param {string} url
+     * @return {Promise<AudioController>}
+     */
+    async load(url) {
+      const data = await fetchArrayBuffer(url);
+      if (data) {
+        this.undecodedBuffer = data;
+        this.loaded = true;
+      } else {
+        console.error(
+          'There wa an error while fetching the audio file, check the network tab for more details'
+        );
+      }
+  
+      return this;
+    }
+  
+    /**
+     * AudioController.initialize is used to instantiate the AudioContext and its buffer. This method should only be
+     * called when AudioController. loaded is true.
+     *
+     * @async
+     * @method initialize
+     * @methodOf AudioController
+     * @param {AudioContextLatencyCategory} latencyHint
+     * @return {Promise<AudioController>}
+     */
+    async initialize(latencyHint = 'interactive', volume = 0.8) {
+      if (!this.loaded) return this;
+  
+      this.context = new AudioContext({ latencyHint });
+      this.context.onstatechange = () => {
+        if (this.context.state !== 'running') this.started = false;
+      };
+      this.buffer = await this.context.decodeAudioData(this.undecodedBuffer.slice());
+  
+      this.createAnalyser();
+      this.createConvolver();
+      this.createGain(volume);
+      this.createSource();
+  
+      this.initialized = true;
+  
+      this.initialTime = this.context.currentTime;
+  
+      return this;
+    }
+  
+    /**
+     * @method createSource
+     * @methodOf AudioController
+     * @return {AudioController}
+     */
+    createSource() {
+      if (!this.context) return this;
+  
+      this.source = this.context.createBufferSource();
+      this.source.buffer = this.buffer;
+  
+      if (this.gain) {
+        this.source.connect(this.gain);
+      } else {
+        this.source.connect(this.context.destination);
+      }
+  
+      return this;
+    }
+  
+    /**
+     * @method createConvolver
+     * @methodOf AudioController
+     * @return {AudioController}
+     */
+    createConvolver() {
+      if (!this.context) return this;
+  
+      this.convolver = this.context.createConvolver();
+      this.convolver.normalize = true;
+      this.convolver.buffer = this.buffer;
+  
+      return this;
+    }
+  
+    /**
+     * @method createAnalyser
+     * @methodOf AudioController
+     * @param {FftSize} fftSize
+     * @return {AudioController}
+     */
+    createAnalyser(fftSize = 2048) {
+      if (!this.context) return this;
+  
+      this.analyser = this.context.createAnalyser();
+  
+      if (fftSize) this.analyser.fftSize = fftSize;
+  
+      this.analyser.connect(this.context.destination);
+  
+      return this;
+    }
+  
+    /**
+     * @method createGain
+     * @methodOf AudioController
+     * @param volume {number}
+     * @return {AudioController}
+     */
+    createGain(volume) {
+      if (!this.context || !this.buffer) return this;
+  
+      this.gain = this.context.createGain();
+      this.gain.gain.value = volume;
+  
+      if (this.analyser) {
+        this.gain.connect(this.analyser);
+      }
+  
+      return this;
+    }
+  
+    setVolume(volume) {
+      if (this.gain) {
+        this.gain.gain.value = volume;
+        this.source.connect(this.gain);
+      }
+    }
+  
+    /**
+     * @method start
+     * @methodOf AudioController
+     * @return {AudioController}
+     */
+    start() {
+      if (this.started || !this.initialized || this.context.state === 'closed') return this;
+  
+      if (this.context.state === 'interrupted' || this.context.state === 'suspended') {
+        this.context.resume().then(() => this.start());
+        return this;
+      }
+  
+      if (this.context.state !== 'running') return this;
+  
+      this.createSource();
+  
+      if (this.realPausedTime) {
+        this.source.start(0, this.realPausedTime);
+      } else {
+        this.realStartTime = this.context.currentTime;
+        this.source.start(0);
+      }
+  
+      let userStopped = false;
+  
+      this.source.onended = async () => {
+        this.started = false;
+        if (!userStopped) {
+          this.realPausedTime = 0;
+          this.realStartTime = 0;
+          if (this.onended) {
+            this.onended();
+          }
+        }
+        userStopped = false;
+      };
+  
+      this.source.userStop = () => {
+        userStopped = true;
+        this.source.stop(0);
+      };
+  
+      this.started = true;
+  
+      return this;
+    }
+  
+    /**
+     * @method stop
+     * @methodOf AudioController
+     * @return {AudioController}
+     */
+    stop() {
+      if (!this.started || !this.initialized || this.context.state === 'closed') return this;
+  
+      this.source.userStop();
+      this.realPausedTime = this.context.currentTime - this.realStartTime;
+  
+      return this;
+    }
+  
+    /**
+     * @method toggle
+     * @methodOf AudioController
+     * @return {AudioController}
+     */
+    toggle() {
+      if (!this.initialized) return this;
+  
+      if (this.started) this.stop();
+      else this.start();
+  
+      return this;
+    }
+  
+    /**
+     * @method analyzeFrequency
+     * @methodOf AudioController
+     * @param {{ fftSize?: FftSize, minFrequency?: number, maxFrequency?: number }} options
+     * @return {Uint8Array | null}
+     */
+    analyzeFrequency(
+      options = {
+        fftSize: undefined,
+        minFrequency: undefined,
+        maxFrequency: undefined,
+      }
+    ) {
+      if (!this.initialized || !this.analyser) return null;
+  
+      let { fftSize = undefined, minFrequency = undefined, maxFrequency = undefined } = options;
+  
+      const sampleRate = this.context.sampleRate;
+  
+      if (!minFrequency) minFrequency = 0;
+      if (!maxFrequency) maxFrequency = sampleRate / 2;
+  
+      if (fftSize) this.analyser.fftSize = fftSize;
+      else fftSize = this.analyser.fftSize;
+  
+      const bufferLength = this.analyser.frequencyBinCount;
+  
+      const dataArray = new Uint8Array(bufferLength);
+      this.analyser.getByteFrequencyData(dataArray);
+  
+      const startIndex = Math.floor((minFrequency / sampleRate) * fftSize);
+      const endIndex = Math.min(Math.floor((maxFrequency / sampleRate) * fftSize), bufferLength);
+  
+      return dataArray.subarray(startIndex, endIndex);
+    }
+  
+    disconnect() {
+      this.analyser?.disconnect();
+      this.convolver?.disconnect();
+      this.gain?.disconnect();
+      this.source?.disconnect();
+    }
+  
+    static create(...args) {
+      try {
+        return new AudioController(...args);
+      } catch (err) {
+        console.error(err);
+        return null;
+      }
+    }
+  }
+  
--- sketch.js
+++ sketch.js
@@ -1,67 +1,18 @@
-class Particle {
-  constructor(pos, vel, lifetime, c) {
-    this.color = c;
-    this.pos = pos;
-    this.vel = vel;
-    this.initialLifetime = lifetime;
-    this.lifetime = lifetime;
-  }
-
-  update() {
-    this.pos.add(this.vel);
-    this.vel.y += 0.08;
-    this.vel.mult(0.99);
-    this.lifetime--;
-  }
-
-  show() {
-    stroke(this.color.r, this.color.g, this.color.b, map(this.lifetime, 0, this.initialLifetime, 0, 255));
-    strokeWeight(3);
-    point(this.pos.x, this.pos.y);
-  }
-}
-
-
-class Explosion {
-  constructor(pos, c, maxLifetime, amount = 50) {
-    this.pos = pos;
-    this.particles = [];
-    this.lifetime = 0;
-    for (let i = 0; i < amount; i++) {
-      const lifetime = random(max(0, maxLifetime - 30), maxLifetime);
-      if (lifetime > this.lifetime) this.lifetime = lifetime;
-      const vel = createVector(random(-3, 3), random(-3, 3));
-      this.particles.push(new Particle(pos.copy(), vel, lifetime, c));
-    }
-    this.finished = false;
-  }
-
-  update() {
-    if (this.lifetime > 0) {
-      for (const p of this.particles) {
-        p.update();
-      }
-      this.lifetime--;
-    } else {
-      this.finished = true;
-    }
-  }
-
-  show() {
-    if (this.lifetime > 0) {
-      for (const p of this.particles) {
-        p.show();
-      }
-    }
-  }
-}
-
 const explosions = [];
-let sound;
-let amp;
+const palette = 'happy';
+const bins = COLOR_PALETTES[palette].length * 3;
+const sample = 1024;
+const realStart = 0;
+const realEnd = sample - (sample / 2);
+const levels = (realEnd - realStart) / bins;
+const threshold = 20;
+const previous = Array.from({ length: bins }).fill([]);
+let sound, amp, fft;
+let size;
+
 
 function preload() {
-  sound = loadSound('./Egzod, Maestro Chives, Neoni - Royalty [NCS Release].mp3');
+  sound = loadSound('audio/JVKE  this is what space feels like Official Lyric Video.mp3');
 }
 
 function setup() {
@@ -69,18 +20,34 @@ function setup() {
   if (canvas) canvas.remove();
   createCanvas(window.innerWidth, window.innerHeight);
   frameRate(30);
+
+  size = (min(width, height) / 2 - 100) / bins;
+
   amp = new p5.Amplitude();
+  fft = new p5.FFT();
+  sound.setVolume(0.5)
 }
 
-const tempAmps = [];
 
 function draw() {
-  background(33);
+  background(22);
+  const spectrum = fft.analyze(sample);
+  // stroke(255);
+  // strokeWeight(2);
+  // for (let i = realStart; i < sample; i++) {
+  //   rect(i, 0, 1, spectrum[i]);
+  // }
+  // line(0, 255, width, 255)
+
   noStroke();
   fill(33);
   rect(0, 0, 35, 30);
   fill(255);
   text(frameRate().toFixed(), 10, 20);
+
+  translate(width / 2, height / 2);
+
+  const levelAMP = amp.getLevel();
 
   for (let i = explosions.length - 1; i >= 0; i--) {
     if (explosions[i].finished) {
@@ -88,31 +55,48 @@ function draw() {
       continue;
     }
 
-    explosions[i].update();
     explosions[i].show();
+    explosions[i].update();
   }
 
+  for (let i = 0; i < bins; i++) {
+    const index = floor(map(i, 0, bins, realStart, realEnd));
+    let levelSUM = 0;
+    for (let j = index; j < index + levels; j++) {
+      levelSUM += spectrum[i];
+    }
+    const levelAVG = map(levelSUM / levels, 0, 255, 0, 1);
+    previous[i].push(levelAVG);
+    if (previous[i].length > threshold) previous[i].splice(0, 1);
+    let previousSUM = 0;
+    for (const prev of previous[i]) {
+      previousSUM += prev;
+    }
+    const previousAVG = previousSUM / previous[i].length;
 
-  let avg = tempAmps.reduce((acc, next) => acc + next, 0) / tempAmps.length;
+    if (levelAVG > previousAVG + map(i, 0, bins, 0.15, 0.01)) {
+      const time =  bins - (i + 1);
+      const level = ceil(map(levelAMP, 0, 1, 1, 50));
+      for (let j = 0; j < level; j++) {
+        const c = COLOR_PALETTES[palette][i % (COLOR_PALETTES[palette].length)].rgba;
+        const iTimes = time * size;
+        const r = random(iTimes, iTimes + 25);
+        const p = p5.Vector.random2D().mult(r);
 
-  let level = amp.getLevel();
-
-  if (level > avg + 0.1) {
-    explosions.push(new Explosion(
-      createVector(width / 2, height / 2),
-      { r: 255, g: 255, b: 255 },
-      50
-    ));
+        explosions.push(new Explosion(
+          p,
+          c,
+          5 + i,
+          3,
+          max(5, (bins - i) / 2)
+        ));
+      }
+    }
   }
-  
-  tempAmps.push(level);
-  if (tempAmps.length > 5) tempAmps.splice(0, 1);
-
-  console.log(level);
 }
 
 function mousePressed() {
-  if (sound.isPlaying()) sound.stop();
+  if (sound.isPlaying()) sound.pause();
   else sound.play();
 }
 
